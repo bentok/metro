3c96c70ed31e39abb5ca62479128b77b2c7e712a
Ben Copeland <bencopeland@macbook-pro.lan>
2020-05-30 12:54:12 -0400

    Setup FSharp library

---
+++ game/MetroFS/Library.fs
@@ -0,0 +1,13 @@
+﻿namespace FSPlayer
+ 
+open Godot
+ 
+type PlayerFs() =
+    inherit KinematicBody2D()
+ 
+    override this._Ready() = 
+        GD.Print("F# is ready")
+        
+    override this._PhysicsProcess (delta) =
+        let velocity = Vector2(300.00f, 0.00f)
+        this.MoveAndSlide(velocity) |> ignore
